0d0bd393a9a0c39ed66af8faf387e15fcda91caa
Chernyshov Igor <chernyshov.ig@yandex.ru>
2022-02-05 18:52:10 +0300

    2 stackView done

--- HW-11/ViewController.swift
+++ HW-11/ViewController.swift
@@ -19,7 +19,16 @@ class ViewController: UIViewController {
         return stackView
     }()
 
-    private lazy var firstStackView = createHorisontalStackView(height: Metric.heightFirstStackView)
+    private lazy var firstStackView = createHorisontalStackView()
+
+    private lazy var firstPointFirstStackView = createHorisontalStackView()
+    private lazy var firstPointTwoStackView = createVerticalStackView()
+    private lazy var firstEmptyOneStackView = createVerticalStackView()
+    private lazy var firstEmptyTwoStackView = createVerticalStackView()
+
+    private lazy var secondStackView =  createHorisontalStackView()
+    private lazy var thirdStackView =  createHorisontalStackView()
+
 
 //     MARK: - Life cycle
 
@@ -37,47 +46,141 @@ class ViewController: UIViewController {
 
     private func setupHierarchy() {
         view.addSubview(parentStackView)
+
+        parentStackView.addArrangedSubview(firstStackView)
+
+        firstStackView.addArrangedSubview(firstPointFirstStackView)
+        firstStackView.addArrangedSubview(firstPointTwoStackView)
+
+        firstPointFirstStackView.addArrangedSubview(ivOne)
+        firstPointTwoStackView.addArrangedSubview(firstEmptyOneStackView)
+        firstPointTwoStackView.addArrangedSubview(myName)
+        firstPointTwoStackView.addArrangedSubview(statusButton)
+        firstPointTwoStackView.addArrangedSubview(onlineLabel)
+        firstPointTwoStackView.addArrangedSubview(firstEmptyTwoStackView)
+
+        parentStackView.addArrangedSubview(secondStackView)
+        secondStackView.addArrangedSubview(bigButton)
+
+        parentStackView.addArrangedSubview(thirdStackView)
     }
 
     private func setupLayout() {
         parentStackView.translatesAutoresizingMaskIntoConstraints = false
+        parentStackView.clipsToBounds = true
 
         parentStackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 23).isActive = true
         parentStackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -23).isActive = true
         parentStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 25).isActive = true
         parentStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
+
+        firstStackView.topAnchor.constraint(equalTo: parentStackView.topAnchor, constant: 0).isActive = true
+        firstStackView.heightAnchor.constraint(equalToConstant: 130).isActive = true
+        firstPointTwoStackView.leftAnchor.constraint(equalTo: firstPointFirstStackView.rightAnchor, constant: 10).isActive = true
+
+        statusButton.centerYAnchor.constraint(equalTo: firstPointFirstStackView.centerYAnchor).isActive = true
+        statusButton.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        statusButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        statusButton.leftAnchor.constraint(equalTo: firstStackView.rightAnchor, constant: -275).isActive = true
+        myName.bottomAnchor.constraint(equalTo: statusButton.topAnchor).isActive = true
+        myName.leftAnchor.constraint(equalTo: firstPointTwoStackView.leftAnchor).isActive = true
+        onlineLabel.topAnchor.constraint(equalTo: statusButton.bottomAnchor).isActive = true
+        onlineLabel.heightAnchor.constraint(equalToConstant: 20).isActive = true
+
+        bigButton.topAnchor.constraint(equalTo: firstStackView.bottomAnchor, constant: 10).isActive = true
+        bigButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+
+
+
+
+        firstPointFirstStackView.widthAnchor.constraint(equalToConstant: 130).isActive = true
     }
 
 //    MARK: - Private func
 
-    private func createHorisontalStackView(height: CGFloat) -> UIStackView {
+    private func createHorisontalStackView() -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = .horizontal
         stackView.distribution = .equalSpacing
-        stackView.heightAnchor.constraint(equalToConstant: height).isActive = true
 
         return stackView
     }
 
-    private func createVerticalStackView(height: CGFloat) -> UIStackView {
+    private func createVerticalStackView() -> UIStackView {
         let stackView = UIStackView()
         stackView.axis = .vertical
-        stackView.distribution = .equalSpacing
-        stackView.heightAnchor.constraint(equalToConstant: height).isActive = true
+//        stackView.distribution = .fillEqually
 
         return stackView
     }
+
+    private let ivOne: UIImageView = {
+        let image = UIImageView(image: .init(named: "Sova"))
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 65
+
+        return image
+    }()
+
+    private lazy var myName: UILabel = {
+        let label = UILabel()
+        label.text = Strings.myName
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 20)
+        label.textAlignment = .left
+
+        return label
+    }()
+
+    private lazy var statusButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle(Strings.statusBut, for: .normal)
+        button.tintColor = Color.systemBlue
+
+        return button
+    }()
+
+    private lazy var onlineLabel: UILabel = {
+        let label = UILabel()
+        label.text = Strings.onlinelabel
+        label.textColor = Color.onlineLabel
+        label.font = .systemFont(ofSize: 12)
+        label.textAlignment = .left
+
+        return label
+    }()
+
+    private lazy var bigButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle(Strings.bigButton, for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = Color.bigButton
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 10
+
+        return button
+    }()
+
+
 //    MARK: - enum
 
     enum Metric {
         static let stackViewSpasing: CGFloat = 23
-        static let heightFirstStackView: CGFloat = 160
     }
 
     enum Color {
         static let background = UIColor(red: 25, green: 25, blue: 27)
+        static let systemBlue = UIColor(red: 115, green: 170, blue: 227)
+        static let onlineLabel = UIColor(red: 126, green: 126, blue: 128)
+        static let bigButton = UIColor(red: 44, green: 45, blue: 46)
     }
 
+    enum Strings {
+        static let myName = "Чернышов Игорь"
+        static let statusBut = "Установить статус"
+        static let onlinelabel = "online 􀟜"
+        static let bigButton = "Редактировать"
+    }
 }
 
 extension UIColor {
